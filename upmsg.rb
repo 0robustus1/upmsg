71364a984d1a130447fbabaec517f16fbf424b94
robustus <robustus@dsone.eu>
2011-04-05 21:12:29 +0200

    Skript objektifiziert. Doku verbessert.
    Nun sind nicht mehr alle Methoden instanzfrei(global), sondern
    die ehemals put_dmesg (jetzt show) Methode sowie daemonize
    gehören zu einem Upmsg-Objekt. Dies sollte zukunftssicherer sein
    als die momentane Variante, und sowieso brauchbarer für die spätere
    GTK-Integration.
    
    Eventuell werden die anderen Methoden in Zukunft in ein Modul
    ausgelagert.

--- upmsg.rb
+++ upmsg.rb
@@ -71,113 +71,130 @@ def fill_space(val)
   return whitespace
 end
 
-#==formatierter dmesg-Output
-#
-#Gibt den dmesg Output formatiert auf der Konsole aus.
-#Es wird implizit vorausgesetzt, dass vorher _get_options_
-#durchgeführt wurde.
-#Wenn nicht wird es Standardformatiert und somit der Timestamp der 
-#Uptime zur Zeit des Events ausgegeben.
-def put_dmesg
-  dmesg = %x[dmesg]
-  if $opt[:fu]
-    now = (%x[cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1 | tr -d "\n"])
-  elsif $opt[:ft]
-    now = Time.new
-    nowstamp = (%x[cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1 | tr -d "\n"])
-
-  else 
-    now = get_actual_uptime
-  end
-  now_length = now.to_s.length
-  dmesg.each_line do |line|
-    time = line[/\[\s*\d+.\d+\]/]
-    if time 
-      time = time[/\d+/]
-      if $opt[:fu]
-        diff = now.to_i - time.to_i
-        time = get_given_uptime(diff)
-      elsif $opt[:ft]
-        diff = nowstamp.to_i - time.to_i
-        time = (now - diff).to_s #weil time sonst Time-Objekt ist
-      else
-        time = get_given_uptime(time) 
-      end
-      space = fill_space(now_length - time.length)
-      puts line.gsub(/\[\s*\d+.\d+\]/ , "[#{space}#{time}]")
-    end
-  end
-end
-
 #==Optionenbehandlung
 #
 #Es existieren folgende Optionen:
-#* keine Option gesetzt: Standardausgabe, wobei Timestamps
-#durch menschenlesbare Werte ersetzt sind:
-#* -i :: Gibt die tatsächliche Uptime am Ende mit aus.
-#* -f(t/u) :: Spezifiert die dmesg Ausgabe erwartet entweder t oder u im Anschluss
-#* -ft :: Gibt hinter jedem Eintrag die tatsächliche Ereigniszeit aus
-#* -fu :: Gibt hinter jedem Eintrag aus wie lange es schon her ist.
-#* -d :: daemon-mode. Es erfolgt keine direkte Ausgabe in die 
-#Konsole, man wird über notfiy's über neue Nachrichten im Kernellog (dmesg)
-#benachrichtigt. Sollten andere Optionen gesetzt sein, werden diese
-#behandelt bevor in den Daemon-Mode gewechselt wird.
+#*  keine Option gesetzt: Standardausgabe, wobei Timestamps
+#   durch menschenlesbare Werte ersetzt sind:
+#*  -i Gibt die tatsächliche Uptime am Ende mit aus.
+#*  -f(t/u) Spezifiert die dmesg Ausgabe erwartet entweder t oder u im Anschluss
+#*  -ft Gibt hinter jedem Eintrag die tatsächliche Ereigniszeit aus
+#*  -fu Gibt hinter jedem Eintrag aus wie lange es schon her ist.
+#*  -d daemon-mode. Es erfolgt keine direkte Ausgabe in die 
+#   Konsole, man wird über notfiy's über neue Nachrichten im Kernellog (dmesg)
+#   benachrichtigt. Sollten andere Optionen gesetzt sein, werden diese
+#   behandelt bevor in den Daemon-Mode gewechselt wird.
 def get_options
-  $opt = Hash.new
+  opt = Hash.new
   ARGV.each do |a|
     arg = a.dup
     if arg.gsub!("-","")
-      arg.each_char {|c| $opt[c.to_sym]=true}
+      arg.each_char {|c| opt[c.to_sym]=true}
     else 
       puts "invalid option."
       exit
     end
-    $opt[:ft]=true if ($opt[:f] && $opt[:t])
-    $opt[:fu]=true if ($opt[:f] && $opt[:u])
-    if $opt[:ft] && $opt[:fu]
+    opt[:ft]=true if (opt[:f] && opt[:t])
+    opt[:fu]=true if (opt[:f] && opt[:u])
+    if opt[:ft] && opt[:fu]
       puts "Only one format-option is allowed."
       exit
     end
   end
+  return opt
 end
 
-#==daemon-Mode
+#==Kernobjekt
 #
-#Startet das Programm im daemon-mode.
-#Es wird in regelmäßigen Abständen(Default 5 Sekunden)
-#auf änderungen in der dmesg-Struktur geprüft. Wenn
-#ja werden diese Änderungen, mit brauchbaren Timestamps versehen,
-#als Notification an den notification-daemon und damit
-#an den Window-Manager gesendet. Es folgt normalerweise
-#eine Bubbleausgabe auf dem Bildschirm.
-def daemonize
-  $dmesg = %x[dmesg]
-  puts "daemon up and running..."
-  while( true ) 
-    sleep 5
-    current = %x[dmesg]
-    Notify.init("dmesg")
-    begin
-      if (current.length > $dmesg.length)
-        text = current[$dmesg.length..(current.length-1)]
-        text = text.gsub(/\[\s*\d+.\d+\]/,'')
-        summary = "dmesg: "+%x[date +"%H:%M:%S"]+"up: "+get_actual_uptime
-        notification = Notify::Notification.new(summary, text, nil)
-        notification.timeout=((2 + (text.count("\n")/2))*1000)
-        notification.show
-      end
-      $dmesg = current
-      current = %x[dmesg]
-      sleep 2
-    end while (current.length > $dmesg.length)
-    Notify.uninit
+#Ein Objekt dieser Klasse repräsentiert ein Laufinstanz für
+#den modifizierten dmesg-Dienst. Er enthält Konfigurationen
+#für die Ausgabe (Wahl der Methode), sowie weitere Einstellungen
+#insbesondere für den daemon-Modus. 
+#
+class Upmsg
+  attr_accessor :opt, :dmesg, :config
+  
+  def initialize(options=Hash.new, config = nil)
+    @opt = options  
+    @config = config
   end
+
+  #===formatierter dmesg-Output
+  #
+  #Gibt den dmesg Output formatiert auf der Konsole aus.
+  #Es wird implizit vorausgesetzt, dass vorher _get_options_
+  #durchgeführt wurde.
+  #Wenn nicht wird es Standardformatiert und somit der Timestamp der 
+  #Uptime zur Zeit des Events ausgegeben.
+  def show
+    dmesg = %x[dmesg]
+    if opt[:fu]
+      now = (%x[cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1 | tr -d "\n"])
+    elsif opt[:ft]
+      now = Time.new
+      nowstamp = (%x[cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1 | tr -d "\n"])
+    else 
+      now = get_actual_uptime
+    end
+    now_length = now.to_s.length
+    dmesg.each_line do |line|
+      time = line[/\[\s*\d+.\d+\]/]
+      if time 
+        time = time[/\d+/]
+        if opt[:fu]
+          diff = now.to_i - time.to_i
+          time = get_given_uptime(diff)
+        elsif opt[:ft]
+          diff = nowstamp.to_i - time.to_i
+          time = (now - diff).to_s #weil time sonst Time-Objekt ist
+        else
+          time = get_given_uptime(time) 
+        end
+        space = fill_space(now_length - time.length)
+        puts line.gsub(/\[\s*\d+.\d+\]/ , "[#{space}#{time}]")
+      end
+    end
+  end
+
+  #===daemon-Mode
+  #
+  #Startet das Programm im daemon-mode.
+  #Es wird in regelmäßigen Abständen(Default 5 Sekunden)
+  #auf Änderungen in der dmesg-Struktur geprüft. Wenn
+  #ja werden diese Änderungen, mit brauchbaren Timestamps versehen,
+  #als Notification an den notification-daemon und damit
+  #an den Window-Manager gesendet. Es folgt normalerweise
+  #eine Bubbleausgabe auf dem Bildschirm.
+  def daemonize
+    dmesg = %x[dmesg]
+    puts "daemon up and running..."
+    while( true ) 
+      sleep 5
+      current = %x[dmesg]
+      Notify.init("dmesg")
+      begin
+        if (current.length > dmesg.length)
+          text = current[dmesg.length..(current.length-1)]
+          text = text.gsub(/\[\s*\d+.\d+\]/,'')
+          summary = "dmesg: "+%x[date +"%H:%M:%S"]+"up: "+get_actual_uptime
+          notification = Notify::Notification.new(summary, text, nil)
+          notification.timeout=((2 + (text.count("\n")/2))*1000)
+          notification.show
+        end
+        dmesg = current
+        current = %x[dmesg]
+        sleep 2
+      end while (current.length > dmesg.length)
+      Notify.uninit
+    end
+  end
+
 end
-get_options 
-put_dmesg if $opt.length == 0
-put_dmesg if $opt[:ft] || $opt[:fu]
-if $opt[:i]
-  put_dmesg
+
+run = Upmsg.new(get_options) 
+run.show if (run.opt.length == 0) || run.opt[:ft] || run.opt[:fu]
+if run.opt[:i]
+  run.show
   puts "Current Uptime: "+get_actual_uptime
 end
-daemonize if $opt[:d]
+daemonize if run.opt[:d]
